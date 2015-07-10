01fc4283bb513d14b459a8dec49a96ac551dcec2
perlancar (@pc-office) <perlancar@gmail.com>
2015-07-10 19:23:13 +0700

    Initial commit

---
+++ lib/App/IndonesianBankingUtils.pm
@@ -0,0 +1,20 @@
+package App::IndonesianFinanceUtils;
+
+use 5.010001;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: CLIs related to Indonesian finance
+
+=head1
+
+This distribution contains several CLI's related to Indonesian finance:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<App::IndonesianBankingUtils>
